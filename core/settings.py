f2b18898225b4be7befba43ecfe4f96d4eae6797
“Divya <divyasajjan@yahoo.co.in>
2026-02-27 13:49:03 -0500

    adding pysqlite to settings and requirements

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
